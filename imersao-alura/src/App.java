469fae9cddbdd8e548b01a67904718accdedb256
Alynne Raquel <alynneraquel@homail.com>
2023-03-30 18:37:17 -0300

    Aula 02

--- imersao-alura/src/App.java
+++ imersao-alura/src/App.java
@@ -1,5 +1,6 @@
-import java.io.IOException;
+import java.io.InputStream;
 import java.net.URI;
+import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
@@ -9,7 +10,7 @@ import java.util.Map;
 
 public class App {
 
-	public static void main(String[] args) throws IOException, InterruptedException {
+	public static void main(String[] args) throws Exception {
 		
 		//fazer uma conexão HTTTP e buscar os top 250 filmes
 		String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
@@ -27,10 +28,19 @@ public class App {
 		System.out.println(listaDeFilmes.size());
 		
 		//Exibir e manipular os dados
+		var geradora = new GeradoraDeFigurinhas();
 		for (Map<String, String> filme : listaDeFilmes) {
+			
+			String urlImagem = filme.get("image");
+			String titulo = filme.get("title");
+			
+			InputStream inputStream = new URL(urlImagem).openStream();
+			String nomeArquivo = titulo + ".png";
+			
+		
+			geradora.cria(inputStream, nomeArquivo);
+			
 			System.out.println(filme.get("title"));
-			System.out.println(filme.get("image"));
-			System.out.println(filme.get("imDbRatingCount"));
 			System.out.println();
 		}
 		
